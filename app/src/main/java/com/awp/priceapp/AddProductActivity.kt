9cb4c1fb3c3ef654f0c17c63d4385b2353423e48
arifwp <arifwahyu696@gmail.com>
2022-06-14 21:02:10 +0700

    add autocomplete in product name

--- app/src/main/java/com/awp/priceapp/AddProductActivity.kt
+++ app/src/main/java/com/awp/priceapp/AddProductActivity.kt
@@ -3,10 +3,16 @@ package com.awp.priceapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
 import com.awp.priceapp.databinding.ActivityAddProductBinding
 import com.awp.priceapp.databinding.ActivityPriceOptimizeBinding
 
 class AddProductActivity : AppCompatActivity() {
+
+    val product = arrayOf("Bottle", "Kemaja", "Celana", "Meja", "Kursi", "Meja Kerja", "Meja Belajar")
 
     private lateinit var binding: ActivityAddProductBinding
 
@@ -16,6 +22,13 @@ class AddProductActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         getSupportActionBar()?.hide()
+
+        val autocomp = binding.autoComplete
+        val adapterArray = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_dropdown_item, product)
+        autocomp.setAdapter(adapterArray)
+        autocomp.setOnItemClickListener(AdapterView.OnItemClickListener { parent, view, position, id ->
+            Toast.makeText(applicationContext, "clicked item = " + product[position], Toast.LENGTH_SHORT).show()
+        })
 
     }
 }
